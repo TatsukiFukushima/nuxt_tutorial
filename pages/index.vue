09a2cb9e98efb433891905fa742ddb290ca1aa8d
TatsukiFukushima <tatsuki2222net@gmail.com>
2019-12-06 15:24:01 +0900

    修正

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@ export default {
       this.content = '';
     },
     find: function() {
-      this.find_flg = this.content != '';
+      this.find_flg = true;
     },
     set_flg: function() {
       if(this.find_flg) {
@@ -62,6 +62,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
